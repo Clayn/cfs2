89457f2d656fd71f8baacd12372180413f486423
Clayn <clayn_osmato@gmx.de>
2019-01-03 16:27:22 +0100

    test
    Added some tests for the directory class
    
    Task #5 - Add unit tests for the local implementation

--- ClaynFileSystem2/ClaynFileSystemTests/src/main/java/net/bplaced/clayn/cfs2/test/VirtualDirectoryTest.java
+++ ClaynFileSystem2/ClaynFileSystemTests/src/main/java/net/bplaced/clayn/cfs2/test/VirtualDirectoryTest.java
@@ -1,7 +1,10 @@
 package net.bplaced.clayn.cfs2.test;
 
+import java.io.IOException;
+import java.util.List;
 import net.bplaced.clayn.cfs2.api.VirtualDirectory;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,17 +15,23 @@ import org.junit.Test;
 public abstract class VirtualDirectoryTest extends BaseFileSystemTest
 {
 
+    private VirtualDirectory root;
+
+    @Before
+    public void initRoot()
+    {
+        root = fileSystem.getRoot();
+    }
+
     @Test
     public void testRootPath()
     {
-        VirtualDirectory root = fileSystem.getRoot();
         Assert.assertEquals("Root path is not '/'", "/", root.getPath());
     }
 
     @Test
     public void testRootParent()
     {
-        VirtualDirectory root = fileSystem.getRoot();
         Assert.assertNull("The root directory must not have any parent",
                 root.getParent());
     }
@@ -30,14 +39,52 @@ public abstract class VirtualDirectoryTest extends BaseFileSystemTest
     @Test
     public void testRootLocation()
     {
-        VirtualDirectory root = fileSystem.getRoot();
         Assert.assertEquals("Root path is not '/'", "/", root.getLocation());
     }
 
     @Test
     public void testRootName()
     {
-        VirtualDirectory root = fileSystem.getRoot();
         Assert.assertEquals("Root path is not '/'", "/", root.getName());
     }
+
+    @Test
+    public void testChangeDirectory()
+    {
+        String name = testName.getMethodName();
+        VirtualDirectory dir = root.changeDirectory(name);
+        Assert.assertFalse(dir.isRoot());
+        Assert.assertTrue(dir.getParent().isRoot());
+        Assert.assertEquals(name, dir.getName());
+        Assert.assertEquals("/" + name, dir.getPath());
+    }
+
+    @Test
+    public void testChangeDirectoryDeep()
+    {
+        String first = testName.getMethodName();
+        String second = first + 2;
+        VirtualDirectory dir = root.changeDirectory(first + "/" + second);
+        Assert.assertFalse(dir.isRoot());
+        Assert.assertFalse(dir.getParent().isRoot());
+        Assert.assertEquals(second, dir.getName());
+        Assert.assertEquals(
+                "Paths don't match. Expected '" + ("/" + first + "/" + second) + "' got '" + dir.getPath() + "'",
+                "/" + first + "/" + second, dir.getPath());
+    }
+
+    @Test
+    public void testListDirectories() throws IOException
+    {
+        List<VirtualDirectory> directories = root.listDirectories();
+        Assert.assertTrue(directories.isEmpty());
+        VirtualDirectory dir = root.changeDirectory("Dir");
+        directories = root.listDirectories();
+        Assert.assertTrue(directories.isEmpty());
+        dir.mkDirs();
+        directories = root.listDirectories();
+        Assert.assertFalse(directories.isEmpty());
+        Assert.assertEquals(dir.getName(), directories.get(0).getName());
+    }
+
 }
